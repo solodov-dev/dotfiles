4d589f860d528a7d3eec53479bec83ad3ec41cd9
Andrey Solodov <solodov.dev@gmail.com>
2020-01-11 00:13:03 +0300

    Update 11.01.2020

--- install.sh
+++ install.sh
@@ -8,8 +8,14 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create folders for dotfiles
+# folder for coc.nvim snippets
 mkdir -p .vim .config/coc/ultisnips
+
+# remove default i3 config file before stowing
 rm .config/i3/config
+
+# make dir for polybar config
+mkdir -p .config/polybar
 
 # stow dotfiles
 stow * 

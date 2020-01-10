6175c4545513fa9c1aaac255d10cb01e49eac1ed
Andrey Solodov <solodov.dev@gmail.com>
2020-01-10 21:47:50 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-sudo pacman -S ttf-fira-code nodejs npm firefox zsh 
+# install packages
+sudo pacman -S git gvim ttf-fira-code nodejs npm firefox zsh 
 
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install vimplug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# create folders for dotfiles
+mkdir -p .vim .config/coc/ultisnips
+
+# stow dotfiles
+stow * 

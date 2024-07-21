2604cc713b08577443e12e568a95052dceb338cc
Солодов Андрей Маркович <a.m.solodov@tinkoff.ru>
2024-07-21 20:30:12 +0300

    Add autopair support

--- nvim/.config/nvim/lua/text.lua
+++ nvim/.config/nvim/lua/text.lua
@@ -1,39 +1,43 @@
+return {
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+	},
 
-return {{
-  'numToStr/Comment.nvim',
-  opts = {
-    -- add any options here
-  },
-  lazy = false,
-},
-
-
-{
-  'nvim-lualine/lualine.nvim',
-  name = "lualine",
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  config = function()
-    require("lualine").setup {
-      options = {
-        -- ... your lualine config
-        theme = 'tokyonight'
-        -- ... your lualine config
-      }
-    }
-  end,
-},
-{
-  "kylechui/nvim-surround",
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-    })
-  end
+	{
+		"nvim-lualine/lualine.nvim",
+		name = "lualine",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					-- ... your lualine config
+					theme = "tokyonight",
+					-- ... your lualine config
+				},
+			})
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
 }
-
-}
-

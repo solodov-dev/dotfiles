524277de4d5c70dfaae0f05660b406f6d3bb9db0
Andrey Solodov <solodov.dev@gmail.com>
2024-08-01 16:53:28 +0300

    neovim ui update

--- nvim/.config/nvim/lua/ui.lua
+++ nvim/.config/nvim/lua/ui.lua
@@ -56,20 +56,4 @@ return { {
       require("telescope").load_extension("undo")
     end,
   },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    }
-  }
-
 }

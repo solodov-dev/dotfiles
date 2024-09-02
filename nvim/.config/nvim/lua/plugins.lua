2385f747e3b295770e15ac2e89b69a352301387c
Andrey Solodov <solodov.dev@gmail.com>
2024-09-02 12:21:34 +0300

    Rust

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,39 +2,38 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local function merge_plugins(...)
-	local plugins = {}
-	for _, tb in ipairs({ ... }) do
-		for _, v in ipairs(tb) do
-			table.insert(plugins, v)
-		end
-	end
+  local plugins = {}
+  for _, tb in ipairs({ ... }) do
+    for _, v in ipairs(tb) do
+      table.insert(plugins, v)
+    end
+  end
 
-	return plugins
+  return plugins
 end
 
 local plugins = merge_plugins(
-	require("dap"),
-	require("files"),
-	require("git"),
-	require("keymaps"),
-	require("lsp"),
-	require("terminal"),
-	require("text"),
-	require("themes"),
-	require("treesitter"),
-	require("ui")
+  require("files"),
+  require("git"),
+  require("keymaps"),
+  require("lsp"),
+  require("terminal"),
+  require("text"),
+  require("themes"),
+  require("treesitter"),
+  require("ui")
 )
 
 require("lazy").setup(plugins)

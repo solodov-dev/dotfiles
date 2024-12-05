f3352a25b7a88aeddfc1fd0955ba9735729e6839
Andrey Solodov <solodov.dev@gmail.com>
2024-12-05 13:56:09 +0300

    Add autocmd close deleted files via oil.nvim

--- nvim/.config/nvim/lua/files.lua
+++ nvim/.config/nvim/lua/files.lua
@@ -1,6 +1,32 @@
-return {{
+-- close deleted files via oil.nvim
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilActionsPost",
+  callback = function(args)
+    local parse_url = function(url)
+      return url:match("^.*://(.*)$")
+    end
+
+    if args.data.err then
+      return
+    end
+
+    for _, action in ipairs(args.data.actions) do
+      if action.type == "delete" and action.entry_type == "file" then
+        local path = parse_url(action.url)
+        local bufnr = vim.fn.bufnr(path)
+        if bufnr == -1 then
+          return
+        end
+
+        vim.cmd("bp | bw " .. bufnr)
+      end
+    end
+  end,
+})
+
+return { {
   'stevearc/oil.nvim',
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
-}}
+} }

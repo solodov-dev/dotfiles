5869e3332e4969aa6e2a9e58d2a4ac41ec683bd4
Andrey Solodov <solodov.dev@gmail.com>
2025-04-17 09:47:15 +0300

    Change theme

--- nvim/.config/nvim/lua/modules/themes.lua
+++ nvim/.config/nvim/lua/modules/themes.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		-- config = function()
-		-- 	vim.cmd("colorscheme tokyonight-night")
-		-- end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		-- config = function()
-		-- 	vim.cmd("colorscheme gruvbox")
-		-- end,
-	},
-	{
-		"neanias/everforest-nvim",
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme everforest")
-		end,
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight-night")
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    -- config = function()
+    --   vim.cmd("colorscheme gruvbox")
+    -- end,
+  },
+  {
+    "neanias/everforest-nvim",
+    priority = 1000,
+    -- config = function()
+    -- 	vim.cmd("colorscheme everforest")
+    -- end,
+  },
 }

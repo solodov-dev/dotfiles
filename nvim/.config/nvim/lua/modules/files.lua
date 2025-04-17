511391842ef2647d6deb88da4517d72a262e937b
Солодов Андрей Маркович <a.m.solodov@tinkoff.ru>
2025-04-17 10:03:40 +0300

    Update tmux theme

--- nvim/.config/nvim/lua/modules/files.lua
+++ nvim/.config/nvim/lua/modules/files.lua
@@ -1,47 +1,47 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require('oil').setup({
-        watch_for_changes = true,
-        view_options = {
-          show_hidden = true,
-        },
-        float = {
-          max_height = 0.3,
-          padding = 0,
-          override = function(conf)
-            conf.relative = 'editor'
-            conf.anchor = 'SW'
-            conf.row = vim.api.nvim_win_get_height(0)
-            return conf
-          end
-        },
-      })
-    end
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {}
-    end,
-  },
-  {
-    'MagicDuck/grug-far.nvim',
-    config = function()
-      require('grug-far').setup({
-        -- options, see Configuration section below
-        -- there are no required options atm
-        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
-      });
-    end
-  },
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				watch_for_changes = true,
+				view_options = {
+					show_hidden = true,
+				},
+				float = {
+					max_height = 0.5,
+					padding = 0,
+					override = function(conf)
+						conf.relative = "editor"
+						conf.anchor = "SW"
+						conf.row = vim.api.nvim_win_get_height(0)
+						return conf
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	{
+		"MagicDuck/grug-far.nvim",
+		config = function()
+			require("grug-far").setup({
+				-- options, see Configuration section below
+				-- there are no required options atm
+				-- engine = 'ripgrep' is default, but 'astgrep' can be specified
+			})
+		end,
+	},
 }

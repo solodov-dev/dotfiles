9ff226548ac9fd87a8a1cecae9a1e7d636c4074a
Andrey Solodov <solodov.dev@gmail.com>
2025-05-13 11:51:33 +0300

    Update format settings for clang-format

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,41 +1,41 @@
 return {
-	{ "williamboman/mason.nvim", version = "^1.0.0" },
-	{ "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local mason = require("mason")
-			local mason_lsp = require("mason-lspconfig")
-			local lspconfig = require("lspconfig")
-			local cmp_lsp = require("cmp_nvim_lsp")
-			mason.setup({})
-			return mason_lsp.setup({
-				ensure_installed = {
-					"awk_ls",
-					"bashls",
-					"clangd",
-					"gopls",
-					"cssls",
-					"cssmodules_ls",
-					"docker_compose_language_service",
-					"dockerls",
-					"emmet_ls",
-					"eslint",
-					"jsonls",
-					"lua_ls",
-					"marksman",
-					"rust_analyzer",
-					"ts_ls",
-					"yamlls",
-				},
-				handlers = {
-					function(server_name)
-						local capabilities = cmp_lsp.default_capabilities()
-						local server = lspconfig[server_name]
-						return server.setup({ capabilities })
-					end,
-				},
-			})
-		end,
-	},
+  { "williamboman/mason.nvim",           version = "^1.0.0" },
+  { "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      mason.setup({})
+      return mason_lspconfig.setup({
+        ensure_installed = {
+          "awk_ls",
+          "bashls",
+          "clangd",
+          "gopls",
+          "cssls",
+          "cssmodules_ls",
+          "docker_compose_language_service",
+          "dockerls",
+          "emmet_ls",
+          "eslint",
+          "jsonls",
+          "lua_ls",
+          "marksman",
+          "rust_analyzer",
+          "ts_ls",
+          "yamlls",
+        },
+        handlers = {
+          function(server_name)
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+            local server = lspconfig[server_name]
+            return server.setup({ capabilities })
+          end,
+        },
+      })
+    end,
+  },
 }

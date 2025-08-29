6c6a8889b7ec6509fa526643d1c624820d5aa48c
Andrey Solodov <solodov.dev@gmail.com>
2025-08-29 09:20:16 +0300

    Dracula official theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,13 +13,13 @@ end
 -- This is where you actually apply your config choices
 -- config.color_scheme_dirs = { "~/.dotfiles/wezterm/colors" }
 -- config.color_scheme = "Everforest Light (Medium)"
-config.color_scheme = "Dracula"
+config.color_scheme = "Dracula (Official)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.window_padding = {
-  left = 0,
-  right = 0,
   top = 0,
   bottom = 0,
+  left = 0,
+  right = 0,
 }
 -- Tab bar
 config.hide_tab_bar_if_only_one_tab = true

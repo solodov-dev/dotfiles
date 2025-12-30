df37c730f0a7e82916bf9fcb11ac57fd37c396ee
Andrey Solodov <solodov.dev@gmail.com>
2025-12-30 11:21:52 +0300

    Gruvbox material theme for neovim and wezterm

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "Mofiqul/dracula.nvim",
-    config = function()
-      return vim.cmd("colorscheme dracula")
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    -- config = function()
-    --   return vim.cmd("colorscheme tokyonight-night")
-    -- end,
-  },
-  {
-    "sainnhe/gruvbox-material",
-    -- config = function()
-    --   return vim.cmd("colorscheme gruvbox-material")
-    -- end,
-  }
+	{
+		"Mofiqul/dracula.nvim",
+		-- config = function()
+		-- 	return vim.cmd("colorscheme dracula")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		-- config = function()
+		--   return vim.cmd("colorscheme tokyonight-night")
+		-- end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			return vim.cmd("colorscheme gruvbox-material")
+		end,
+	},
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,9 +12,8 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
--- config.color_scheme_dirs = { "~/.dotfiles/wezterm/colors" }
--- config.color_scheme = "Everforest Light (Medium)"
-config.color_scheme = "Dracula (Official)"
+-- config.color_scheme = "Dracula (Official)"
+config.color_scheme = "Gruvbox Material (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.window_padding = {
 	top = 0,

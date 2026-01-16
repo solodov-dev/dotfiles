a9b71e76e13433ec61230319eb838cc48952baf7
Andrey Solodov <solodov.dev@gmail.com>
2026-01-16 10:51:51 +0300

    Neovim: remove plugins dependent on treesitter because of breaking changes

--- nvim/.config/nvim/lua/plugins/iswap.lua
+++
@@ -1,1 +0,0 @@
-return { "mizlan/iswap.nvim" }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,31 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local ts = require("nvim-treesitter.configs")
-			return ts.setup({
-				highlight = { enable = true },
-				indent = { enable = true },
-				ensure_installed = {
-					"rust",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"css",
-					"javascript",
-					"html",
-					"typescript",
-					"markdown",
-					"bash",
-					"tsx",
-					"c",
-					"clojure",
-					"dockerfile",
-					"yaml",
-				},
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter.config")
+      return ts.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "rust",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "css",
+          "javascript",
+          "html",
+          "typescript",
+          "markdown",
+          "bash",
+          "tsx",
+          "c",
+          "clojure",
+          "dockerfile",
+          "yaml",
+        },
+      })
+    end,
+  },
 }

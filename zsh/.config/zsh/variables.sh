48b217f7703ae204fd8679dac278b90debfc4fc3
Солодов Андрей Маркович <a.m.solodov@tinkoff.ru>
2026-03-23 11:25:15 +0300

    Set normal XDG variables

--- zsh/.config/zsh/variables.sh
+++ zsh/.config/zsh/variables.sh
@@ -2,8 +2,8 @@ export EDITOR='vim'
 export VISUAL='vim'
 
 export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$XDG_CONFIG_HOME/local/share"
-export XDG_CACHE_HOME="$XDG_CONFIG_HOME/cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 #HISTORY
 export HISTFILE="$HOME/.bash_history"   # History filepath
